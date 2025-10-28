08c3c3df3276d557fe1315e7d567371d38beb25b
Manuel Alvarez Sanchez <malvasan@hotmail.com>
2025-10-28 18:23:55 -0500

    feature: Grid created by a function,  reused for the button at top. This function adds the change color functionality for each div and dinamic height depending on size of grid.

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const body = document.querySelector("body");
+const container = document.querySelector("#container");
+const button = document.querySelector("#newGrid")
+
+function createGrid(numberSquares = 16){
+    for (let i = 0; i < numberSquares; i++) {
+        let div = document.createElement("div");
+        div.classList.toggle("flex-container");
+        for (let o = 0; o < numberSquares; o++) {
+            let square = document.createElement("div");
+            square.classList.toggle("square");
+            square.style.height = `${960 / numberSquares}px`;
+            square.addEventListener('mouseover',(e)=>{
+                square.style.background = 'red';
+            })
+            div.appendChild(square);
+        }
+        container.appendChild(div);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", createGrid(16));
+
+button.addEventListener("click", (e)=>{
+    const userInput = prompt("How big should I be?");
+    const numberValue = parseInt(userInput);
+
+    if(isNaN(numberValue)){
+        console.log("It should be a number.")
+    }else{
+        if(numberValue > 100 || numberValue < 1){
+            console.log("The biggest number is 100")
+        }else{
+            container.innerHTML = '';
+            createGrid(numberValue);
+        }
+        
+    }
+
+})
